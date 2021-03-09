006521f97f6d42a137d7b7e5e71a6ac4717fe561
prebenn <42801690+prebenn@users.noreply.github.com>
2021-03-09 01:42:48 +0100

    lazy strict check of rej

--- content.js
+++ content.js
@@ -39,11 +39,79 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   const berAfr = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(1) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(5) > td:nth-child(7) > span > span").innerHTML;
   const totalAfr = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(1) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tfoot > tr > td:nth-child(6)").innerHTML;
 
-  const trdRej = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(2) > td.measure.calculation.field-align-right > span > span").innerHTML;
-  const trmRej = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(4) > td.measure.calculation.field-align-right > span > span").innerHTML;
-  const stvRej = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(3) > td.measure.calculation.field-align-right > span > span").innerHTML;
-  const berRej = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(5) > td.measure.calculation.field-align-right > span > span").innerHTML;
-  const oslRej = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(1) > td.measure.calculation.field-align-right > span > span").innerHTML;
+  const rej1 = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(1)").innerText;
+  const rej2 = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(2)").innerText;
+  const rej3 = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(3)").innerText;
+  const rej4 = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(4)").innerText;
+  const rej5 = document.querySelector("#dashboard > div > div.dashboard-elements > div > div > div > div > div.grid > div:nth-child(2) > div > lk-vis-element > div > div > div.vis-wrapper-container > div > div > div > lk-visualization-container > div > div > div > div > div > div.lk-vis-table-main-wrapper > table > tbody > tr:nth-child(5)").innerText;
+
+  let oslRej;
+  let trdRej;
+  let trmRej;
+  let stvRej;
+  let berRej;
+
+  // yea yea DRY but it's almost 2am
+
+  if(rej1.includes("Oslo")) {
+    oslRej = rej1.slice(-5);
+  } else if (rej2.includes("Oslo")) {
+    oslRej = rej2.slice(-5);
+  } else if (rej3.includes("Oslo")) {
+    oslRej = rej3.slice(-5);
+  } else if (rej4.includes("Oslo")) {
+    oslRej = rej4.slice(-5);
+  } else if (rej5.includes("Oslo")) {
+    oslRej = rej5.slice(-5);
+  }
+
+  if(rej1.includes("Trondheim")) {
+    trdRej = rej1.slice(-5);
+  } else if (rej2.includes("Trondheim")) {
+    trdRej = rej2.slice(-5);
+  } else if (rej3.includes("Trondheim")) {
+    trdRej = rej3.slice(-5);
+  } else if (rej4.includes("Trondheim")) {
+    trdRej = rej4.slice(-5);
+  } else if (rej5.includes("Trondheim")) {
+    trdRej = rej5.slice(-5);
+  }
+
+  if(rej1.includes("Tromsø")) {
+    trmRej = rej1.slice(-5);
+  } else if (rej2.includes("Tromsø")) {
+    trmRej = rej2.slice(-5);
+  } else if (rej3.includes("Tromsø")) {
+    trmRej = rej3.slice(-5);
+  } else if (rej4.includes("Tromsø")) {
+    trmRej = rej4.slice(-5);
+  } else if (rej5.includes("Tromsø")) {
+    trmRej = rej5.slice(-5);
+  }
+
+  if(rej1.includes("Stavanger")) {
+    stvRej = rej1.slice(-5);
+  } else if (rej2.includes("Stavanger")) {
+    stvRej = rej2.slice(-5);
+  } else if (rej3.includes("Stavanger")) {
+    stvRej = rej3.slice(-5);
+  } else if (rej4.includes("Stavanger")) {
+    stvRej = rej4.slice(-5);
+  } else if (rej5.includes("Stavanger")) {
+    stvRej = rej5.slice(-5);
+  }
+
+  if(rej1.includes("Bergen")) {
+    berRej = rej1.slice(-5);
+  } else if (rej2.includes("Bergen")) {
+    berRej = rej2.slice(-5);
+  } else if (rej3.includes("Bergen")) {
+    berRej = rej3.slice(-5);
+  } else if (rej4.includes("Bergen")) {
+    berRej = rej4.slice(-5);
+  } else if (rej5.includes("Bergen")) {
+    berRej = rej5.slice(-5);
+  }
 
 
   // and a SLICK one line response with the sendResponse method, if I do say so myself. sending the data we collected back to popup to be drawn 
